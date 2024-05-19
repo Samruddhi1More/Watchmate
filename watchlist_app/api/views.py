f6099470949160584bc801bb557561fc2d916859
Samruddhi-More <samruddhi.more@pyrack.com>
2024-05-19 19:22:17 +0530

    User Model

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,8 +1,50 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from watchlist_app.models import StreamPlatform, WatchList
-from watchlist_app.api.serializers import StreamPlatformSerializer, WatchListSerializer
+from rest_framework import generics
+from rest_framework import mixins
+
+from watchlist_app.models import StreamPlatform, WatchList, Review
+from watchlist_app.api.serializers import StreamPlatformSerializer, WatchListSerializer, ReviewSerializer
+
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        watchlist = WatchList.objects.get(pk=pk)
+
+        serializer.save(watchlist=watchlist)
+
+class ReviewList(generics.ListAPIView):
+    # queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
+    
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+# class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+# class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+    
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 class StreamPlatformAV(APIView):
     
